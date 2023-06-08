33b74c52cd949b2aca5098ba9ddcba4523e46660
shaikh Azharuddin <112998169+azhar8888@users.noreply.github.com>
2023-06-08 15:12:33 +0530

    this is a carry-overs app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd'
+
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar />
+
+      </div>
+
+      <div className='main'>
+        <Layout>
+
+          <div className='routes'>
+
+
+
+
+            <Routes>
+              <Route exact path='/' element={<Homepage />} />
+
+
+              <Route exact path='/exchnages' element={<Exchanges />} />
+
+
+              <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />} />
+
+
+              <Route exact path='/crypto/:coinId' element={<CryptoDetails />} />
+
+
+              <Route exact path='/news' element={<News />} />
+            </Routes>
+
+
+           
+
+
+
+          </div>
+
+        </Layout>
+        <div className='footer' >
+          <Typography.Title level={5} style={{color:'white' ,  textAlign:'center'}}>
+            Cryptoverse <br></br>
+            All Right Reserved
+
+          </Typography.Title>
+         
+      </div>
+
+
+
+      </div>
+
+
+
     </div>
   );
 }
